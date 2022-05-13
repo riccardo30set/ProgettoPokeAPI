5f56045dc2af7236f6bd6b782ba93d76a91cb9e3
Mazzo <mazzocchimarco@istitutocorni.it>
2022-05-13 17:33:14 +0200

    cambiato il font

--- PokeAPI/app/src/main/java/com/example/progettopokeapi/ListActivity.java
+++ PokeAPI/app/src/main/java/com/example/progettopokeapi/ListActivity.java
@@ -40,19 +40,19 @@ public class ListActivity extends AppCompatActivity {
         this.RestCallPokemonNames();
         //ATTENZIONE, questi nomi sono qui perche la rest call viene eseguita su un thread a parte, quindi capita che l'adapter carichi l'array prima che venga modificato.
 
-        nome[0]="bulbasaur";
-        nome[1]="ivysaur";
-        nome[2]="venusaur";
-        nome[3]="charmander";
-        nome[4]="charmeleon";
-        nome[5]="charizard";
-        nome[6]="squirtle";
-        nome[7]="wartortle";
-        nome[8]="blastoise";
-        nome[9]="caterpie";
-        nome[10]="metapod";
-        nome[11]="butterfree";
-        nome[12]="weedle";
+        nome[0]="Bulbasaur";
+        nome[1]="Ivysaur";
+        nome[2]="Venusaur";
+        nome[3]="Charmander";
+        nome[4]="Charmeleon";
+        nome[5]="Charizard";
+        nome[6]="Squirtle";
+        nome[7]="Wartortle";
+        nome[8]="Blastoise";
+        nome[9]="Caterpie";
+        nome[10]="Metapod";
+        nome[11]="Butterfree";
+        nome[12]="Weedle";
 
         for(int i=1; i<899;i++){
             id="pokemon"+i;
@@ -75,13 +75,7 @@ public class ListActivity extends AppCompatActivity {
         listaPokemon.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-
                 startActivity(intent);
-
-
-
-
-
             }
         });
 
@@ -98,6 +92,7 @@ public class ListActivity extends AppCompatActivity {
                                 for(int i=1;i<899;i++){
                                     try {
                                         nome[i-1]=response.getJSONArray("pokemon_entries").getJSONObject(i-1).getJSONObject("pokemon_species").getString("name");
+                                        nome[i-1]=nome[i-1].substring(0,1).toUpperCase()+nome[i-1].substring(1,nome[i-1].length());
                                     } catch (JSONException e) {
                                         e.printStackTrace();
                                     }
